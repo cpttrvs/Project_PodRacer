18e05945e3853f2828337f9d7c48b711740a9516
cpttrvs <cpt_trivius@hotmail.fr>
2018-11-23 17:54:33 +0100

    prototype de déplacement au clavier (ZD et OK)

---
+++ Project_PodRacer/Assets/Scripts/Controller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Pod))]
+public class Controller : MonoBehaviour {
+
+	//inputs
+	bool leapMotion = false;
+	float[] intensity = {0f, 0f}; // between 0 and 1
+
+	//pod
+	Pod pod;
+
+	// Use this for initialization
+	void Start () {
+		pod = GetComponent<Pod>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//reduce speed
+		intensity[0] -= Time.deltaTime;
+		intensity[1] -= Time.deltaTime;
+
+		if(leapMotion) {
+			LeapMotionInput();
+		} else {
+			KeyboardInput();
+		}
+
+		intensity[0] = (intensity[0] * 10f) / 10f;
+		intensity[1] = (intensity[1] * 10f) / 10f;
+
+		if(intensity[0] > 1) intensity[0] = 1f;
+		if(intensity[0] < 0) intensity[0] = 0f;
+		if(intensity[1] > 1) intensity[1] = 1f;
+		if(intensity[1] < 0) intensity[1] = 0f;
+
+		Debug.Log("Controller : L(" + intensity[0] +") R("+ intensity[1] + ")");
+		pod.Move(intensity);
+	}
+
+	void KeyboardInput() {
+		intensity[0] = Input.GetAxis("LeftPod");
+		intensity[1] = Input.GetAxis("RightPod");
+	}
+
+	void LeapMotionInput() {
+	}
+}
---
+++ Project_PodRacer/Assets/Scripts/Pod.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Pod : MonoBehaviour {
+	Rigidbody rigidBody;
+	// thrusters
+	[SerializeField] float speed = 1f;
+	[SerializeField] float tiltSpeed = 10f;
+	[SerializeField] float thrusterSpacing = 1.0f;
+	float[] thrusterIntensity = {0.0f, 0.0f};
+
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 oppositeVelocity = -rigidBody.velocity;
+   		rigidBody.AddRelativeForce(oppositeVelocity);
+
+		Vector3 oppositeAngularVelocity = -rigidBody.angularVelocity;
+		rigidBody.AddRelativeTorque(oppositeAngularVelocity);
+	
+		//ClampRotation();
+	}
+
+	public void Move(float[] intensity) {
+		Vector3 direction = new Vector3(0,0,0);
+		if(intensity[0] == intensity[1]) {
+			direction = new Vector3(0, 0, intensity[0]);
+			direction = transform.forward * intensity[0];
+			//transform.position += transform.forward * intensity[0] * speed;
+			rigidBody.AddRelativeForce(direction * speed,ForceMode.Impulse);
+		}
+
+		if(intensity[0] > intensity[1]) {
+			direction = new Vector3(0, -intensity[0], -intensity[0]);
+			rigidBody.AddRelativeTorque(direction * tiltSpeed);
+		}
+
+		if(intensity[1] > intensity[0]) {
+			direction = new Vector3(0, intensity[1], intensity[1]);
+			rigidBody.AddRelativeTorque(direction * tiltSpeed);
+		}
+
+		Debug.Log("Pod : " + direction.ToString());
+
+	}
+
+	void ClampRotation() {
+		Vector3 currentRotation = transform.localRotation.eulerAngles;
+		currentRotation.y = Mathf.Clamp(currentRotation.y, -40, 40);
+		transform.localRotation = Quaternion.Euler(currentRotation);
+	}
+}
