2118d117cb645197678756bf847805aa306ed7dd
cpttrvs <cpt_trivius@hotmail.fr>
2018-11-30 17:36:48 +0100

    tilt en cours

--- Project_PodRacer/Assets/Scripts/Pod.cs
+++ Project_PodRacer/Assets/Scripts/Pod.cs
@@ -7,9 +7,10 @@ public class Pod : MonoBehaviour {
 	Rigidbody rb;
 	// thrusters
 	[SerializeField] float speedModifier = 3f;
-	[SerializeField] float rotateModifier = 10f;
+	[SerializeField] float tiltModifier = 3f;
 	float speed = 1f;
 	float currentAngle = 0f;
+	float currentTilt = 0f;
 	float[] thrusterIntensity = {0.0f, 0.0f};
 
 
@@ -22,9 +23,19 @@ public class Pod : MonoBehaviour {
 	void FixedUpdate () {
 		//Vector3 oppositeVelocity = -rb.velocity;
    		//rb.AddForce(oppositeVelocity);
+
+		
+
+		if(currentTilt > 0)
+			currentTilt -= Time.deltaTime;
+		if(currentTilt < 0) 
+			currentTilt += Time.deltaTime;
+		transform.Rotate(new Vector3(0,0,1) * currentTilt);
 	}
 
 	public void Move(float[] intensity) {
+		thrusterIntensity[0] = intensity[0];
+		thrusterIntensity[1] = intensity[1];
 		float angleLeft = intensity[0];
 		float angleRight = intensity[1];
 
@@ -33,15 +44,15 @@ public class Pod : MonoBehaviour {
 			if(angleLeft == angleRight) {
 				// forward
 				speed = angleRight * speedModifier;
-
-				rb.AddForce(transform.forward * speed, ForceMode.Impulse);
 			} else {
 				if(angleLeft > angleRight) {
 					// left higher, so turn right
 					currentAngle = (angleLeft - angleRight);
+					currentTilt = -(Mathf.Abs(intensity[0] - intensity[1]));
 				} else if (angleRight > angleLeft) {
 					// right higher, so turn left
 					currentAngle = -(angleRight - angleLeft);
+					currentTilt = Mathf.Abs(intensity[0] - intensity[1]);
 				}
 
 				float tempLeft = angleLeft; float tempRight = angleRight;
@@ -49,11 +60,9 @@ public class Pod : MonoBehaviour {
 				if(tempRight < 0f) tempRight = 0f;
 				speed = Mathf.Abs(tempLeft - tempRight)/speedModifier;
 				
-				rb.AddForce(transform.forward * speed, ForceMode.Impulse);
-				transform.Rotate(transform.up * currentAngle);
+				transform.Rotate(Vector3.up * currentAngle);
 			}
+			rb.AddForce(transform.forward * speed, ForceMode.Impulse);
 		}
-
-		
 	}
 }
